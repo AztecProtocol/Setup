9f9c7d22ebbce9f160f80ccce24198b0f3a934bc
Charlie Lye <karl.lye@gmail.com>
2019-10-27 11:37:54 +0000

    Bump build class.

--- setup-iac/main.tf
+++ setup-iac/main.tf
@@ -105,7 +105,7 @@ resource "aws_security_group_rule" "public_allow_ssh" {
   from_port         = 22
   to_port           = 22
   protocol          = "tcp"
-  cidr_blocks       = ["82.163.119.138/32", "217.169.11.246/32", "94.197.120.55/32", "188.29.0.0/16", "92.40.0.0/16"]
+  cidr_blocks       = ["82.163.119.138/32", "217.169.11.246/32", "94.197.120.55/32", "188.29.0.0/16", "92.40.0.0/16", "86.4.50.193/32"]
   security_group_id = "${aws_security_group.public.id}"
 }
 
