7d2112ea7c8d2a121a5be034a85f6175215dead3
Charlie Lye <karl.lye@gmail.com>
2019-10-29 06:32:53 +0000

    Pull latest deps.

--- ci-scripts/build.sh
+++ ci-scripts/build.sh
@@ -2,9 +2,13 @@
 set -e
 
 IMAGE_NAME=$1
-REGISTRY=${2:-278380418400.dkr.ecr.eu-west-2.amazonaws.com}
 
-docker build -t $REGISTRY/$IMAGE_NAME:latest .
+# Docker layer caching can have old layers. Always pull given images from remote.
+for DEP in $2; do
+  docker pull $DEP
+done
+
+docker build -t $IMAGE_NAME:latest .
 if [ -n "$CIRCLE_SHA1" ]; then
-  docker tag $REGISTRY/$IMAGE_NAME:latest $REGISTRY/$IMAGE_NAME:$CIRCLE_SHA1
+  docker tag $IMAGE_NAME:latest $IMAGE_NAME:$CIRCLE_SHA1
 fi
