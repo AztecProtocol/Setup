79c6213ec494620896037bc6d4e7c7a9bd5e8b89
Charlie Lye <karl.lye@gmail.com>
2019-10-30 13:32:26 +0000

    Only pull remote images if we haven't rebuilt in this run.

--- ci-scripts/build.sh
+++ ci-scripts/build.sh
@@ -3,12 +3,16 @@ set -e
 
 IMAGE_NAME=$1
 
-# Docker layer caching can have old layers. Always pull given images from remote.
+# Docker layer caching can have old layers. Pull given images from remote if we haven't rebuilt this run.
 for DEP in $2; do
-  docker pull $DEP
+  if [ ! -f /tmp/${DEP#*/}.rebuilt ]; then
+    docker pull $DEP:latest
+  fi
 done
 
 docker build -t $IMAGE_NAME:latest .
 if [ -n "$CIRCLE_SHA1" ]; then
   docker tag $IMAGE_NAME:latest $IMAGE_NAME:$CIRCLE_SHA1
 fi
+
+touch /tmp/${IMAGE_NAME#*/}.rebuilt
