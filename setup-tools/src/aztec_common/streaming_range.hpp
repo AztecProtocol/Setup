3818b30e219d7beaf3c8cffde8051dd9a0d578b0
zac-williamson <blorktronics@gmail.com>
2020-01-31 02:00:01 +0000

    removed comments

--- setup-tools/src/aztec_common/streaming_range.hpp
+++ setup-tools/src/aztec_common/streaming_range.hpp
@@ -17,7 +17,7 @@ bb::g1::affine_element decompress(const bb::fq::field_t& x_in)
 {
     bb::fq::field_t uncompressed = x_in;
     bool y_bit_flag = (uncompressed.data[3] >> 63ULL) == 1ULL;
-    // printf("y_bit_flag = %u \n", y_bit_flag ? 1 : 0);
+
     uncompressed.data[3] = uncompressed.data[3] & 0x7fffffffffffffffULL;
     bb::fq::field_t x;
     bb::fq::to_montgomery_form(uncompressed, x);
@@ -50,8 +50,6 @@ bb::g1::affine_element read_bberg_element_from_buffer(char *buffer)
     bb::fq::field_t x_buf;
 
 
-    //  = &elements[i].X;
-    //  = &elements[i].Y;
     memcpy(&x_buf, buffer, sizeof(bb::fq::field_t));
 
     if (isLittleEndian())
@@ -60,8 +58,6 @@ bb::g1::affine_element read_bberg_element_from_buffer(char *buffer)
         x.data[2] = __bswap_64(x_buf.data[1]);
         x.data[1] = __bswap_64(x_buf.data[2]);
         x.data[0] = __bswap_64(x_buf.data[3]);
-
-        // __bswap_bigint<num_limbs>(x);
     }
     else
     {
@@ -82,7 +78,7 @@ void read_bberg_elements_to_file(bb::g1::affine_element* elements, char *buffer,
 {
     const size_t bytes_per_element = sizeof(bb::fq::field_t);
     size_t num_elements = buffer_size / bytes_per_element;
-    // elements.reserve(elements.size() + num_elements);
+
     for (size_t i = 0; i < num_elements; ++i)
     {
         elements[i] = read_bberg_element_from_buffer(&buffer[i * bytes_per_element]);
@@ -116,30 +112,10 @@ void read_file(std::string range_path, std::vector<bb::g1::affine_element>& poin
                 g1_buffer_size = 32; // only 1 point here
             }
             std::string filename = range_path + "data" + std::to_string(i * POINTS_PER_RANGE_FILE) + ".dat";
-            //  std::cout << "filename = " << filename << std::endl;
+
             auto buffer = read_file_into_buffer(filename);
             read_bberg_elements_to_file(&points[i * POINTS_PER_RANGE_FILE], &buffer[0], g1_buffer_size, true);
         }
     }
-    // for (size_t i = 0; i < num_files; ++i)
-    // {
-    //     if (i % 100 == 0)
-    //     {
-    //         printf("i = %lu \n", i);
-    //     }
-    //     // printf("points size = %lu \n", points.size());
-    // }
 }
 }
-
-// 0x0118c4d5b837bcc2bc89b5b398b5974e9f5944073b32078b7e231fec938883b0
-// 0x0118c4d5b837bcc2bc89b5b398b5974e9f5944073b32078b7e231fec938883b0
-
-// 0x260e01b251f6f1c7e7ff4e580791dee8ea51d87a358e038b4efe30fac09383c1
-// 0x260e01b251f6f1c7e7ff4e580791dee8ea51d87a358e038b4efe30fac09383c1
-
-// 0x22febda3c0c0632a11e6dd3f96e6cea211e6dd3f96e6cea2854a87d4dacc5e55
-// 0x22febda3c0c0632a56475b4214e5615e11e6dd3f96e6cea2854a87d4dacc5e55
-
-// 0x04fc6369f7110fe3d25156c1bb9a72859cf2a04641f99ba4ee413c80da6a5fe4
-// 0x04fc6369f7110fe3d25156c1bb9a7285ee413c80da6a5fe4ee413c80da6a5fe4
